cb03784d914dc5b7ea47cd1c566bbc5bd7df8106
linkaiyan <1760942033@qq.com>
2025-05-10 02:43:14 +0800

    feat：修改

--- packages/lkyCCC/index.js
+++ packages/lkyCCC/index.js
@@ -3,13 +3,13 @@
  * @Author: linkaiyan
  * @Email: linkaiyan@xinyu668.com
  * @Date: 2025-04-02 14:17:41
- * @LastEditTime: 2025-04-07 11:04:47
+ * @LastEditTime: 2025-05-10 02:43:05
  * @LastEditors: linkaiyan
  * @Description: 
  */
 import LButton from './components/LButton'
 
-const components = [LButton]
+const components = [LButton];
 
 const install = (app) => {
   components.forEach(component => {
